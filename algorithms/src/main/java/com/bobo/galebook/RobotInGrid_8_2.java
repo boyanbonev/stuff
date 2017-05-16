cf2f929a6349ee6b1b37623c6636c66ca6020121
boyanbonev <bobo.bonev@gmail.com>
2017-05-16 23:45:04 +0300

    Fix the backtracking algorithm for searching path in a grid
    There was a small issue that I fixed. Something else what I did was to add
    memoization for the implementation so that the alg. complexity is now
    O(rows*cols) instead of O(2^(rows+cols)). I also refactored the code to be
    a bit more OOP like.
    
    Tested manually by running the alg. with different inputs.

--- algorithms/src/main/java/com/bobo/galebook/RobotInGrid_8_2.java
+++ algorithms/src/main/java/com/bobo/galebook/RobotInGrid_8_2.java
@@ -3,111 +3,168 @@
  ************************************************************************/
 package com.bobo.galebook;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang3.Validate;
 
 public class RobotInGrid_8_2 {
 
-   private static Point end;
+	public static void main(String[] args) {
+		int n = 50;
+		Set<Point> obstacles = getObstacles(n);
 
-   public static void main(String[] args) {
-      boolean[][] grid = generateGrid(10, 10);
-      end = new Point(9, 9);
-      Point start = new Point(0, 0);
+		Point startPoint = new Point(0, 0);
+		Point endPoint = new Point(n - 1, n - 1);
 
-      List<Point> path = findPath(grid, start);
-      System.out.println("Path with len=" + path.size() + " found: " + path);
-   }
+		Grid grid = new Grid(n, n, obstacles, startPoint, endPoint);
 
-   private static boolean[][] generateGrid(int cols, int rows) {
-      boolean[][] grid = new boolean[rows][cols];
-      for (int i = 0; i < grid.length; i++) {
-         for (int j = 0; j < grid[i].length; j++) {
-            grid[i][j] = true;
-         }
-      }
-      putObstacles(grid);
-      return grid;
-   }
+		long startTime = System.currentTimeMillis();
+		List<Point> path = findPath(grid);
+		System.out.println("Path found for " + (System.currentTimeMillis() - startTime) + "ms");
+		System.out.println("Path with len=" + path.size() + " found: " + path);
+	}
 
-   private static void putObstacles(boolean[][] grid) {
-      int cols = grid[0].length;
-      for (int i = 0; i < grid.length - 1; i++) {
-         grid[cols - i - 1][i] = false;
-      }
-   }
+	private static Set<Point> getObstacles(int n) {
+		Set<Point> obstacles = new HashSet<>();
+		for (int i = 0; i < n - 1; i++) {
+			obstacles.add(new Point(n - i - 1, i));
+		}
 
-   public static List<Point> findPath(boolean[][] grid, Point startingPoint) {
-      if (grid == null || grid.length == 0) {
-         return null;
-      }
+		return obstacles;
+	}
 
-      List<Point> path = new LinkedList<>();
-      findPath(grid, startingPoint, path);
+	public static List<Point> findPath(Grid grid) {
+		if (grid == null) {
+			return Collections.emptyList();
+		}
 
-      return path;
-   }
+		List<Point> path = new LinkedList<>();
+		findPath(grid, grid.getStartPoint(), path);
 
-   public static boolean findPath(boolean[][] grid, Point currentPoint, List<Point> path) {
-      if (isPointNotOk(currentPoint, grid)) {
-         return false;
-      }
+		// the result is in reversed order, so fix this
+		Collections.reverse(path);
 
-      if (currentPoint.equals(end) ||
-            findPath(grid, currentPoint.moveDown(), path) ||
-            findPath(grid, currentPoint.moveRight(), path)) {
-         path.add(currentPoint);
-      }
+		return path;
+	}
 
-      return false;
-   }
+	public static boolean findPath(Grid grid, Point currentPoint,
+			List<Point> path) {
+		if (grid.isPointVisited(currentPoint)) {
+			return false;
+		}
+		if (!grid.isPointInGrid(currentPoint)) {
+			grid.visitPoint(currentPoint);
+			return false;
+		}
 
-   private static boolean isPointNotOk(Point point, boolean[][] grid) {
-      return point.row >= grid.length ||
-            point.row < 0 || // row
-            point.col >= grid[0].length ||
-            point.col < 0 || // col
-            !grid[point.row][point.col];
-   }
+		if (currentPoint.equals(grid.getEndPoint()) || findPath(grid, currentPoint.moveDown(), path)
+				|| findPath(grid, currentPoint.moveRight(), path)) {
+			path.add(currentPoint);
+			return true;
+		}
 
-   static class Point {
-      private int row, col;
+		grid.visitPoint(currentPoint);
+		return false;
+	}
 
-      public Point(int row, int col) {
-         this.row = row;
-         this.col = col;
-      }
+	static class Point {
+		private int row, col;
 
-      public int getCol() {
-         return col;
-      }
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
 
-      public int getRow() {
-         return row;
-      }
+		public int getCol() {
+			return col;
+		}
 
-      @Override
-      public String toString() {
-         return "(" + row + "," + col + ")";
-      }
+		public int getRow() {
+			return row;
+		}
 
-      @Override
-      public boolean equals(Object obj) {
-         if (!(obj instanceof Point)) {
-            // instanceof makes null check
-            return false;
-         }
+		@Override
+		public String toString() {
+			return "(" + row + "," + col + ")";
+		}
 
-         Point other = (Point) obj;
-         return this.row == other.row && this.col == other.col;
-      }
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Point)) {
+				// instanceof makes null check
+				return false;
+			}
 
-      public Point moveRight() {
-         return new Point(row, col + 1);
-      }
+			Point other = (Point) obj;
+			return this.row == other.row && this.col == other.col;
+		}
 
-      public Point moveDown() {
-         return new Point(row + 1, col);
-      }
-   }
+		@Override
+		public int hashCode() {
+			return Integer.hashCode(row) * 13 + Integer.hashCode(col);
+		}
+
+		public Point moveRight() {
+			return new Point(row, col + 1);
+		}
+
+		public Point moveDown() {
+			return new Point(row + 1, col);
+		}
+	}
+
+	static class Grid {
+		private final int gridRows;
+		private final int gridCols;
+		private final Set<Point> obstacles;
+		private final Point startPoint;
+		private final Point endPoint;
+		private final Set<Point> visited;
+
+		public Grid(int gridRows, int gridCols, Set<Point> obstacles, Point startPoint, Point endPoint) {
+
+			Validate.isTrue(gridRows > 0);
+			Validate.isTrue(gridCols > 0);
+			Validate.notNull(obstacles);
+			Validate.notNull(startPoint);
+			Validate.notNull(endPoint);
+
+			this.gridRows = gridRows;
+			this.gridCols = gridCols;
+			this.obstacles = obstacles;
+			this.startPoint = startPoint;
+			this.endPoint = endPoint;
+			visited = new HashSet<>();
+		}
+
+		public Point getStartPoint() {
+			return startPoint;
+		}
+
+		public Point getEndPoint() {
+			return endPoint;
+		}
+
+		public void visitPoint(Point point) {
+			visited.add(point);
+		}
+
+		public boolean isPointVisited(Point point) {
+			return visited.contains(point);
+		}
+
+		public boolean isPointInGrid(Point point) {
+			if (point == null) {
+				return false;
+			}
+
+			return point.getCol() >= 0 && point.getCol() < gridCols && point.getRow() >= 0
+					&& point.getRow() < gridRows && !obstacles.contains(point);
+
+		}
+	}
 }
