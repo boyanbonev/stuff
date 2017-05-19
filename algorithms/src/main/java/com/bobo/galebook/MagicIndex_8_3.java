e5e472f2ee0b998a03970709ff09eaa5e9ae4ef9
boyanbonev <bobo.bonev@gmail.com>
2017-05-19 22:00:29 +0300

    Fix the Magic index implementation
    This is an optimized solution of the magic index. It works the similar way
    as the binary search. Something else is that it works even if there are
    duplicate array elements. The only requirement is that the array elements
    are sorted.

--- algorithms/src/main/java/com/bobo/galebook/MagicIndex_8_3.java
+++ algorithms/src/main/java/com/bobo/galebook/MagicIndex_8_3.java
@@ -4,22 +4,48 @@
 package com.bobo.galebook;
 
 public class MagicIndex_8_3 {
-   public static void main(String[] args) {
-      int[] arr = new int[] { 1, 0, 2, 4 };
-      int index = findMagicIndex(arr);
-      if (index != -1) {
-         System.out.println("Found index = " + index);
-      }
+	public static void main(String[] args) {
+		int[] arr = new int[] { -1, 2, 3, 4, 4 };
+		int index = fastFindMagicIndex(arr);
+		if (index != -1) {
+			System.out.println("Found index = " + index);
+		} else {
+			System.out.println("No magic index");
+		}
 
-   }
+	}
 
-   public static int findMagicIndex(int[] arr) {
-      for (int i = 0; i < arr.length; i++) {
-         if (arr[i] == i) {
-            return i;
-         }
-      }
+	public static int findMagicIndex(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == i) {
+				return i;
+			}
+		}
 
-      return -1;
-   }
+		return -1;
+	}
+
+	public static int fastFindMagicIndex(int[] arr) {
+		return fastFindMagicIndex(arr, 0, arr.length);
+	}
+
+	public static int fastFindMagicIndex(int[] arr, int start, int end) {
+		if (start > end) {
+			return -1;
+		}
+		int midIndex = (start + end) / 2;
+		int midElem = arr[midIndex];
+		if (midElem == midIndex) {
+			return midElem;
+		}
+
+		int leftInd = Math.min(midIndex - 1, midElem);
+		int left = fastFindMagicIndex(arr, start, leftInd);
+		if (left > 0) {
+			return left;
+		}
+
+		int rightInd = Math.max(midIndex + 1, midElem);
+		return fastFindMagicIndex(arr, rightInd, end);
+	}
 }
